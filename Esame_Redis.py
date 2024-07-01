82dd9415b8906d545e42d29a12fcbb3d3a83260b
ZaccagniDavide <davide.zaccagni@itsrizzoli.it>
2024-07-01 14:08:03 +0200

    Create Esame_Redis.py
    add Eseme_Redis, connection redis and functions Register & Login

---
+++ Esame_Redis.py
@@ -0,0 +1,19 @@
+import redis
+import hashlib
+from datetime import datetime
+
+# Connessione al database Redis
+r = redis.StrictRedis(
+    host='redis-14639.c8.us-east-1-3.ec2.redns.redis-cloud.com',
+    port=14639,
+    db=0,
+    decode_responses=True,
+    password='3Xy7jwkrKb1WpzoDsRNR0jNQqLcQeeQa'
+)
+
+def register(username, password):
+    if r.hexists('users', username):
+        return 'Username already exists'
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    r.hset('users', username, hashed_password)
+    return 'User registered successfully'
